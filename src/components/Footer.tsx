a93f247cedb5fa24c506fd8592284a11f4b39265
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-10-24 15:25:08 +0100

    Add footer to website

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer(): JSX.Element {
+  return (
+    <>
+      <p>
+        Sources for Images: <a href="https://unsplash.com/"> unsplash</a> and <a href="https://www.pexels.com/"> pexels.</a>
+      </p>
+      <p>
+        <small>Powered by The Academy</small>
+      </p>
+    </>
+  );
+}
+
+export default Footer;
