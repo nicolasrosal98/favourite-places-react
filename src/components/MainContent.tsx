0c98327fe9b2754725f1aa45976270d969f3b6bc
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-10-24 15:47:44 +0100

    Add pictures to entries2

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -7,7 +7,7 @@ function MainContent(): JSX.Element {
         title={"Sleeping in the clouds"}
         placeName={"La Mesa de Los Santos"}
         countryName={"Colombia"}
-        img={"./img/refugiolaroca.jpg"}
+        img={"https://images.pexels.com/photos/545964/pexels-photo-545964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}
         googleLocation={
           "https://www.google.com/maps/place/Refugio+La+Roca/@6.7804515,-73.0883802,17z/data=!3m1!4b1!4m8!3m7!1s0x8e69b557b88a88cf:0x2dc77bdd91fcaa6a!5m2!4m1!1i2!8m2!3d6.7804515!4d-73.0861915?hl=en-US"
         }
@@ -19,7 +19,7 @@ function MainContent(): JSX.Element {
         title={"Medieval Old Town"}
         placeName={"Edinburgh"}
         countryName={"Scotland"}
-        img={"./img/edinburgh.jpg"}
+        img={"https://images.pexels.com/photos/4596565/pexels-photo-4596565.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}
         googleLocation={
           "https://www.google.com/maps/place/Edinburgh,+UK/@55.9411289,-3.3454216,11z/data=!3m1!4b1!4m5!3m4!1s0x4887b800a5982623:0x64f2147b7ce71727!8m2!3d55.953252!4d-3.188267?hl=en-US"
         }
@@ -31,7 +31,7 @@ function MainContent(): JSX.Element {
         title={"Under the stars"}
         placeName={"Valle Del Elqui"}
         countryName={"Chile"}
-        img={""}
+        img={"https://images.pexels.com/photos/33825/milky-way-rocks-night-landscape.jpg?auto=compress&cs=tinysrgb&h=750&w=1260"}
         googleLocation={
           "https://www.google.com/maps/place/Pisco+Elqui,+Paiguano,+Coquimbo,+Chile/@-30.1273213,-70.5293051,12z/data=!4m5!3m4!1s0x969001c5b1ed97f9:0x10ea851a657ed26e!8m2!3d-30.124214!4d-70.495571"
         }
@@ -43,7 +43,7 @@ function MainContent(): JSX.Element {
         title={"The birth place of DWS"}
         placeName={"Cala Santanyí"}
         countryName={"Spain"}
-        img={"./img/calasantanyi.jpg"}
+        img={"https://images.pexels.com/photos/2803288/pexels-photo-2803288.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}
         googleLocation={
           "https://www.google.com/maps?q=cala+santanyi&source=lmns&bih=595&biw=1035&hl=en-US&sa=X&ved=2ahUKEwi6itGamuPzAhWAQEEAHU4CDyMQ_AUoAnoECAEQAg"
         }
