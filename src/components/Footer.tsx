3357184da5ab53c7fd005ecbb38becdb5dc704b3
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-10-24 15:26:05 +0100

    Format website footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,8 @@ function Footer(): JSX.Element {
   return (
     <>
       <p>
-        Sources for Images: <a href="https://unsplash.com/"> unsplash</a> and <a href="https://www.pexels.com/"> pexels.</a>
+        Sources for Images: <a href="https://unsplash.com/"> unsplash</a> and{" "}
+        <a href="https://www.pexels.com/"> pexels.</a>
       </p>
       <p>
         <small>Powered by The Academy</small>
