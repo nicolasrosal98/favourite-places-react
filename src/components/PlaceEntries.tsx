4b1684b5cdfb82478a5b6c4ca4a6cf2683532e9b
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-10-24 14:09:23 +0100

    Fix format issue in PlaceEntries.tsx

--- src/components/PlaceEntries.tsx
+++ src/components/PlaceEntries.tsx
@@ -1,21 +1,26 @@
 interface PlaceEntry {
-    title: string;
-    placeName: string;
-    countryName: string;
-    img: string;
-    googleLocation: string;
-    explanation: string;
-};
+  title: string;
+  placeName: string;
+  countryName: string;
+  img: string;
+  googleLocation: string;
+  explanation: string;
+}
 
-function PlaceEntries(props:PlaceEntry): JSX.Element {
-    return (
-        <>
-            <img src = {props.img} alt = {props.placeName}></img>
-            <h1><strong>{props.title}</strong></h1>
-            <h2><strong>{props.placeName}</strong>(<a href={props.googleLocation}>map link</a>)</h2>
-            <p>{props.explanation}</p>
-        </>
-    );
-};
+function PlaceEntries(props: PlaceEntry): JSX.Element {
+  return (
+    <>
+      <img src={props.img} alt={props.placeName}></img>
+      <h1>
+        <strong>{props.title}</strong>
+      </h1>
+      <h2>
+        <strong>{props.placeName}</strong>(
+        <a href={props.googleLocation}>map link</a>)
+      </h2>
+      <p>{props.explanation}</p>
+    </>
+  );
+}
 
 export default PlaceEntries;
